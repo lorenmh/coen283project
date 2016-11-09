16b83a37e94f5161f47ee4d09d951a82855d56fd
loren <neutronstar@gmail.com>
2016-11-09 11:51:43 -0800

    made NonConcurrent.java dead simple

--- NonConcurrent.java
+++ NonConcurrent.java
@@ -1,12 +1,5 @@
-public class NonConcurrent extends Thread{
-	public NonConcurrent(int id) {
-	}
-
-	//when start() is called on each thread, will print Hello message.
-	public void run() {
-	}
-
-	public static void main(String[] args) throws InterruptedException {
+public class NonConcurrent{
+	public static void main(String[] args) {
 		for (int i = 0; i < 4; ++i) {
 			System.out.println("Hello from iteration " + i);
 		}
